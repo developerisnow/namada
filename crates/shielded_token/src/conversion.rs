0c814cad1307c40c83087b887c090a25d4a07035
brentstone <brentstone01@gmail.com>
2024-02-28 00:26:04 -0500

    update tingz

--- crates/shielded_token/src/conversion.rs
+++ crates/shielded_token/src/conversion.rs
@@ -6,9 +6,6 @@ use namada_core::dec::Dec;
 use namada_core::uint::Uint;
 use namada_parameters as parameters;
 use namada_storage::{StorageRead, StorageWrite};
-use namada_trans_token::inflation::{
-    ShieldedRewardsController, ShieldedValsToUpdate,
-};
 use namada_trans_token::storage_key::{balance_key, minted_balance_key};
 use namada_trans_token::{read_denom, Amount, DenominatedAmount, Denomination};
 
